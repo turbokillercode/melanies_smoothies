fdf6bc1f93377f26d1e9774cbd42a07597187bc1
turbokillercode <tyler.doolittle85@gmail.com>
2026-06-09 08:33:50 -0700

    Update streamlit_app.py
    Expose json for smoothiefroot

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
