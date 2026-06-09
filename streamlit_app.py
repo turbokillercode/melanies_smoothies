86571b4c85ba88981fb89fa1cc60bcb53ee47c2a
turbokillercode <tyler.doolittle85@gmail.com>
2026-06-09 08:38:03 -0700

    Update streamlit_app.py
    Starting to integrate smoothiefroot into fruits_chosen block, to eventually return the correct information for what was chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import os
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -30,6 +31,8 @@ if ingredients_list:
 
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
+        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -43,8 +46,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-import requests  
-smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
