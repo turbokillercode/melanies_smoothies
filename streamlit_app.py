fdfca0b2233d9444030067548176645c3d77792e
turbokillercode <tyler.doolittle85@gmail.com>
2026-06-09 08:39:32 -0700

    Update streamlit_app.py
    Integrating dynamic api call per fruit_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ if ingredients_list:
 
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
-        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/watermelon")  
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/" + fruit_chosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
